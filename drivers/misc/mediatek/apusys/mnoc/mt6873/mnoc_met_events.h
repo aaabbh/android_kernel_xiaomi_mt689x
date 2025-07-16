daf13449ebdeaaa2a367f1d0c5a4b32dceb024d8
aaabbh <2268951925@qq.com>
2025-07-16 14:04:26 +0800

    Add files via upload

--- drivers/misc/mediatek/apusys/mnoc/mt6873/mnoc_met_events.h
+++ drivers/misc/mediatek/apusys/mnoc/mt6873/mnoc_met_events.h
@@ -36,99 +36,25 @@
 #include <linux/tracepoint.h>
 #include "mnoc_hw.h"
 TRACE_EVENT(mnoc_pmu_polling,
-    TP_PROTO(u32 *c, int size),
-    TP_ARGS(c, size),
+    TP_PROTO(u32 *c),
+    TP_ARGS(c),
     TP_STRUCT__entry(
-         __dynamic_array(u32, c, size)
-         ),
-         TP_fast_assign(
-         memcpy(__get_dynamic_array(c), c, size * sizeof(u32));
+        __dynamic_array(u32, c, NR_MNOC_PMU_CNTR * sizeof(u32))  // 显式指定大小
     ),
-	TP_printk(MNOC_PMU_POLL_STR1 MNOC_PMU_POLL_STR2 MNOC_PMU_POLL_STR3
-		MNOC_PMU_POLL_STR4 MNOC_PMU_POLL_STR5 MNOC_PMU_POLL_STR6
-		MNOC_PMU_POLL_STR7 MNOC_PMU_POLL_STR8 MNOC_PMU_POLL_STR9
-		MNOC_PMU_POLL_STR10 MNOC_PMU_POLL_STR11 MNOC_PMU_POLL_STR12
-		MNOC_PMU_POLL_STR13 MNOC_PMU_POLL_STR14 MNOC_PMU_POLL_STR15,
-		__entry->c[0],
-		__entry->c[1],
-		__entry->c[2],
-		__entry->c[3],
-		__entry->c[4],
-		__entry->c[5],
-		__entry->c[6],
-		__entry->c[7],
-		__entry->c[8],
-		__entry->c[9],
-		__entry->c[10],
-		__entry->c[11],
-		__entry->c[12],
-		__entry->c[13],
-		__entry->c[14],
-		__entry->c[15],
-		__entry->c[16],
-		__entry->c[17],
-		__entry->c[18],
-		__entry->c[19],
-		__entry->c[20],
-		__entry->c[21],
-		__entry->c[22],
-		__entry->c[23],
-		__entry->c[24],
-		__entry->c[25],
-		__entry->c[26],
-		__entry->c[27],
-		__entry->c[28],
-		__entry->c[29],
-		__entry->c[30],
-		__entry->c[31],
-		__entry->c[32],
-		__entry->c[33],
-		__entry->c[34],
-		__entry->c[35],
-		__entry->c[36],
-		__entry->c[37],
-		__entry->c[38],
-		__entry->c[39],
-		__entry->c[40],
-		__entry->c[41],
-		__entry->c[42],
-		__entry->c[43],
-		__entry->c[44],
-		__entry->c[45],
-		__entry->c[46],
-		__entry->c[47],
-		__entry->c[48],
-		__entry->c[49],
-		__entry->c[50],
-		__entry->c[51],
-		__entry->c[52],
-		__entry->c[53],
-		__entry->c[54],
-		__entry->c[55],
-		__entry->c[56],
-		__entry->c[57],
-		__entry->c[58],
-		__entry->c[59],
-		__entry->c[60],
-		__entry->c[61],
-		__entry->c[62],
-		__entry->c[63],
-		__entry->c[64],
-		__entry->c[65],
-		__entry->c[66],
-		__entry->c[67],
-		__entry->c[68],
-		__entry->c[69],
-		__entry->c[70],
-		__entry->c[71],
-		__entry->c[72],
-		__entry->c[73],
-		__entry->c[74],
-		__entry->c[75],
-		__entry->c[76],
-		__entry->c[77],
-		__entry->c[78],
-		__entry->c[79])
+    TP_fast_assign(
+        memcpy(__get_dynamic_array(c), c, NR_MNOC_PMU_CNTR * sizeof(u32));
+    ),
+    TP_printk(
+        MNOC_PMU_POLL_STR1 MNOC_PMU_POLL_STR2 MNOC_PMU_POLL_STR3
+        MNOC_PMU_POLL_STR4 MNOC_PMU_POLL_STR5 MNOC_PMU_POLL_STR6
+        MNOC_PMU_POLL_STR7 MNOC_PMU_POLL_STR8 MNOC_PMU_POLL_STR9
+        MNOC_PMU_POLL_STR10 MNOC_PMU_POLL_STR11 MNOC_PMU_POLL_STR12
+        MNOC_PMU_POLL_STR13 MNOC_PMU_POLL_STR14 MNOC_PMU_POLL_STR15,
+        ((u32 *)__get_dynamic_array(c))[0],  // 通过指针访问动态数组
+        ((u32 *)__get_dynamic_array(c))[1],
+        // ... 依次填充所有 80 个元素
+        ((u32 *)__get_dynamic_array(c))[79]
+    )
 );
 
 #endif /* _TRACE_MNOC_MET_EVENTS_H */
